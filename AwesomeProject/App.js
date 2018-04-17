e613b62ad19d88513bf68cdd2bfb9c07ad704fd4
SoniGama <abinyuuu@gmail.com>
2018-04-17 11:17:39 -0500

    agregando componentes de criptomonedas y divisas

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -1,6 +1,3 @@
-// http://data.fixer.io/api/latest?access_key=869a4e85e4c8db07005c445355ba0b7e
-// https://api.coinmarketcap.com/v1/ticker/
-
 import React, { Component } from 'react';
 import { AppRegistry, StyleSheet, Text, View } from 'react-native';
 import Greeting from './components/Greeting';
