5c14864dee00a07c0c1702e6a9b77b750b19f761
SoniGama <abinyuuu@gmail.com>
2018-04-17 14:39:55 -0500

    cambios divisa js

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -1,17 +1,25 @@
 import React, { Component } from 'react';
 import { AppRegistry, StyleSheet, Text, View } from 'react-native';
 import Greeting from './components/Greeting';
+import Divisa from './components/Divisa';
 
 export default class LotsOfGreetings extends Component {
   render() {
     return (
       <View>
-        <Greeting name='Rexxar' />
-        <Greeting name='Jaina' />
-        <Greeting name='Valeera' />
-        {/* <Prueba /> */}
+        <Greeting name='Ivan' />
+        {/* <Divisa /> */}
       </View>
     );
   }
 }
 
+const styles = StyleSheet.create({
+  container: {
+    color: 'blue',
+    fontWeight: 'bold',
+    fontSize: 30,
+    textAlign: 'center',
+    marginTop: 60,
+  },
+});
